ae4717b5ec8c1ea3ec63e5e44cd57f3a72aacd4c
kockono <kockonoop@gmail.com>
2022-06-05 22:03:49 -0600

    Se agrego promesas en el menu del dashboard

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -12,7 +12,8 @@ export class SidebarService {
       submenu:[
         { titulo: 'Main', url: '/'},
         { titulo: 'ProgressBar', url: 'progress'},
-        { titulo: 'Gráficas', url: 'grafica1'}
+        { titulo: 'Gráficas', url: 'grafica1'},
+        { titulo: 'Promesad', url: 'promesas'}
       ]
 
     }
